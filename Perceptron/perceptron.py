9dc12b5717dc3edcf06e4fab0d032e0ad1891632
Vyacheslav Trushkov <slvtrshkv@gmail.com>
2024-09-15 18:56:47 +0200

    part of operation was vectorized

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Perceptron:
     def __init__(self, eta=0.1):
         self.w = None
@@ -5,11 +7,11 @@ class Perceptron:
         self.eta = eta
 
     def predict(self, x):
-        net_input = sum(w * x_i for w, x_i in zip(self.w, x)) + self.b
+        net_input = self.w.T @ x + self.b
         return 1 if net_input > 0 else 0
 
     def fit(self, X, Y, max_iter=1000):
-        self.w = [0.0] * len(X[0])
+        self.w = np.random.random(len(X))
         self.b = 0.0
         
         for _ in range(max_iter):
@@ -19,7 +21,7 @@ class Perceptron:
                 diff = y - y_hat
                 if diff != 0:
                     errors += 1
-                    self.w = [w + self.eta * diff * x for w, x in zip(self.w, x_i)]
+                    self.w += self.eta * diff * x_i
                     self.b += self.eta * diff
             if errors == 0:
                 break
@@ -32,8 +34,8 @@ class Perceptron:
 
 if __name__ == "__main__":
     perceptron = Perceptron(eta=0.1)
-    X = [[140, 0], [10, 1]]  # 140 cm (not red), 10 cm (red)
-    Y = [1, 0]  # 1 is banana, 0 is apple
+    X = np.array([[140, 0], [10, 1]])  # 140 cm (not red), 10 cm (red)
+    Y = np.array([1, 0])  # 1 is banana, 0 is apple
     perceptron.fit(X, Y)
-    print(perceptron.predict([100, 0]))
-    print(perceptron.print_me())
+    print(perceptron.predict(np.array([100, 0])))
+    print(perceptron.print_me()) 
